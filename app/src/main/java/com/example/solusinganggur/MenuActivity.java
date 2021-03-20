e346bcd6c31eec83fa127e1e5cba3ef984e260c0
Ramdan Rohendi <ramdanrohendi@gmail.com>
2021-03-20 10:05:01 +0700

    Fixed Bottom Navigation

--- app/src/main/java/com/example/solusinganggur/MenuActivity.java
+++ app/src/main/java/com/example/solusinganggur/MenuActivity.java
@@ -6,70 +6,79 @@ import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class DashboardActivity extends AppCompatActivity {
-    ImageView btnNotif;
-    ImageView btnHome;
-    ImageView btnSearch;
-    ImageView btnChat;
-    ImageView btnProfile;
+public class MenuActivity extends AppCompatActivity {
+    LinearLayout navHome;
+    LinearLayout navSearch;
+    LinearLayout navMessage;
+    LinearLayout navProfile;
+    TextView txtHome;
+    TextView txtSearch;
+    TextView txtMessage;
+    TextView txtProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_menu);
 
-        btnNotif = findViewById(R.id.notif);
-        btnHome = findViewById(R.id.home);
-        btnSearch = findViewById(R.id.search);
-        btnChat = findViewById(R.id.chat);
-        btnProfile = findViewById(R.id.akun);
+        navHome = findViewById(R.id.nav_home);
+        navSearch = findViewById(R.id.nav_search);
+        navMessage = findViewById(R.id.nav_message);
+        navProfile = findViewById(R.id.nav_profile);
 
-        btnHome.setImageResource(R.drawable.homehitam);
+        txtHome = findViewById(R.id.txthome);
+        txtSearch = findViewById(R.id.txtsearch);
+        txtMessage = findViewById(R.id.txtmessage);
+        txtProfile = findViewById(R.id.txtprofile);
+
+        navDefault();
+        txtHome.setText("Home");
         loadFragment(new HomeFragment());
     }
 
     public void keNotif(View view) {
-        gbrDefault();
-        btnNotif.setImageResource(R.drawable.notifhitam);
+        navDefault();
+//        btnNotif.setImageResource(R.drawable.notifhitam);
         Fragment fragment = new NotificationsFragment();
         loadFragment(fragment);
     }
 
     public void keHome(View view) {
-        gbrDefault();
-        btnHome.setImageResource(R.drawable.homehitam);
+        navDefault();
+        txtHome.setText("Home");
         Fragment fragment = new HomeFragment();
         loadFragment(fragment);
     }
 
     public void keSearch(View view) {
-        gbrDefault();
-        btnSearch.setImageResource(R.drawable.searchhitam);
+        navDefault();
+        txtSearch.setText("Search");
         Fragment fragment = new SearchFragment();
         loadFragment(fragment);
     }
 
     public void keChat(View view) {
-        gbrDefault();
-        btnChat.setImageResource(R.drawable.chathitam);
+        navDefault();
+        txtMessage.setText("Message");
         Fragment fragment = new ChatFragment();
         loadFragment(fragment);
     }
 
     public void keProfile(View view) {
-        gbrDefault();
-        btnProfile.setImageResource(R.drawable.accounthitam);
+        navDefault();
+        txtProfile.setText("Profile");
         Fragment fragment = new ProfileFragment();
         loadFragment(fragment);
     }
 
-    public void gbrDefault(){
-        btnNotif.setImageResource(R.drawable.notif);
-        btnHome.setImageResource(R.drawable.homeputih);
-        btnSearch.setImageResource(R.drawable.searchputih);
-        btnChat.setImageResource(R.drawable.chatputih);
-        btnProfile.setImageResource(R.drawable.accountputih);
+    public void navDefault(){
+        txtHome.setText("");
+        txtSearch.setText("");
+        txtMessage.setText("");
+        txtProfile.setText("");
     }
 
     public boolean loadFragment(Fragment fragment) {
