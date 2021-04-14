7fe0692921fb5c1c01a3b82d29dee99dd53d672b
AnisaNoorSyami <syamianisa3@gmail.com>
2021-04-14 12:37:22 +0700

    menambah layout message, notifikasi, dan edit profile

---
+++ app/src/main/java/com/example/solusinganggur/Perusahaan_editprofileActivity.java
@@ -0,0 +1,14 @@
+package com.example.solusinganggur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Perusahaan_editprofileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perusahaan_editprofile);
+    }
+}
