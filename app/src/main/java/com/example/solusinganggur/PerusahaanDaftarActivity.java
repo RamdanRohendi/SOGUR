589d2d408745b0827a48d58105f517af0fb903a7
rizkyfird <rizkyfirdaus7000@gmail.com>
2021-04-15 21:27:29 +0700

    Register Perusahaan Validation

--- app/src/main/java/com/example/solusinganggur/PerusahaanDaftarActivity.java
+++ app/src/main/java/com/example/solusinganggur/PerusahaanDaftarActivity.java
@@ -1,25 +1,86 @@
 package com.example.solusinganggur;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+
 public class PerusahaanDaftarActivity extends AppCompatActivity {
-    Button btnDaftar;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
+    private EditText txtNamaPerusahaan;
+    private TextInputLayout inputLayoutNamaPerusahaan;
+    private EditText txtEmailPerusahaan;
+    private TextInputLayout inputLayoutEmailPerusahaan;
+    private EditText txtAlamatPerusahaan;
+    private TextInputLayout inputLayoutAlamatPerusahaan;
+    private EditText txtPassword;
+    private TextInputLayout inputLayoutPassword;
+    private EditText txtConfirmPassword;
+    private TextInputLayout inputLayoutConfirmPassword;
+    private EditText txtTentangPerusahaan;
+    private Button btnDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perusahaan_daftar);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        txtNamaPerusahaan = findViewById(R.id.isiNamaPerusahaan);
+        inputLayoutNamaPerusahaan = findViewById(R.id.inputnamaperusahaan);
+        txtEmailPerusahaan = findViewById(R.id.isiEmailPerusahaan);
+        inputLayoutEmailPerusahaan = findViewById(R.id.inputemailperusahaan);
+        txtAlamatPerusahaan = findViewById(R.id.isiAlamatPerusahaan);
+        inputLayoutAlamatPerusahaan = findViewById(R.id.inputalamatperusahaan);
+        txtPassword = findViewById(R.id.isiPassword);
+        inputLayoutPassword = findViewById(R.id.inputpassword);
+        txtConfirmPassword = findViewById(R.id.isiConfirmPassword);
+        inputLayoutConfirmPassword = findViewById(R.id.inputconfirmpassword);
 
         btnDaftar = findViewById(R.id.daftar_perusahaan);
         btnDaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), AuthLoginActivity.class));
+
+                if (txtNamaPerusahaan.getText().toString().trim().isEmpty()) {
+                    inputLayoutNamaPerusahaan.setError("Nama Perusahaan tidak boleh kosong");
+                    return;
+                }
+
+                if (txtEmailPerusahaan.getText().toString().trim().isEmpty()) {
+                    inputLayoutEmailPerusahaan.setError("Email Perusahaan tidak boleh kosong");
+                    return;
+                }
+
+                if (txtPassword.getText().toString().trim().isEmpty()) {
+                    inputLayoutPassword.setError("Password tidak boleh kosong");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(txtConfirmPassword.getText().toString())) {
+                    inputLayoutConfirmPassword.setError("Masukkan Password Konfirmasi");
+
+                    if (!txtConfirmPassword.equals(txtPassword)) {
+                        inputLayoutConfirmPassword.setError("Password tidak sama");
+                    }
+                }
             }
         });
     }
@@ -27,4 +88,5 @@ public class PerusahaanDaftarActivity extends AppCompatActivity {
     public void kembali(View view) {
         finish();
     }
+
 }
