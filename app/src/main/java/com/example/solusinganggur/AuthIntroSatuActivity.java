0de17c3620b94214d2d56934a40f015dbae14331
gabitn66-sudo <6emilan66@gmail.com>
2021-04-21 00:10:20 +0700

    Slide inihin iyeukeun

--- app/src/main/java/com/example/solusinganggur/AuthIntroSatuActivity.java
+++ app/src/main/java/com/example/solusinganggur/AuthIntroSatuActivity.java
@@ -1,24 +1,48 @@
 package com.example.solusinganggur;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.LayoutInflaterCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.solusinganggur.adapter.IntroSlidePageAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AuthIntroSatuActivity extends AppCompatActivity {
     private TextView skip;
     private ImageView gbr;
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_intro1);
+
+        List<Fragment> list = new ArrayList<>();
+        list.add(new IntroSatuFragment());
+        list.add(new IntroDuaFragment());
+
+        pager = findViewById(R.id.pager);
+        pagerAdapter = new IntroSlidePageAdapter(getSupportFragmentManager(), list);
+
+        pager.setAdapter(pagerAdapter);
 
         skip = findViewById(R.id.skip);
         skip.setOnClickListener(new View.OnClickListener() {
@@ -60,4 +84,5 @@ public class AuthIntroSatuActivity extends AppCompatActivity {
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
     }
+
 }
